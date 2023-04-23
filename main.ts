cc9642cc322403d9d22aebeae192fb5404d16dfc
cdiareg310 <131588857+cdiareg310@users.noreply.github.com>
2023-04-23 12:47:14 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    NÚMERO += 1
-    basic.showNumber(NÚMERO)
-    if (NÚMERO == 9) {
-        NÚMERO = 0
+    for (let index = 0; index < 9; index++) {
+        NÚMERO += 1
         basic.showNumber(NÚMERO)
+        basic.pause(500)
     }
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    basic.pause(2000)
+    maqueen.motorStop(maqueen.Motors.All)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
 })
 let NÚMERO = 0
 NÚMERO = 0
